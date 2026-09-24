f63d78ce968790c18a548ab1e56c1987a466d21f
tsaibingyi <tsaibingyi@gmail.com>
2026-09-24 18:27:38 +0800

    CardSet獨立

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 #include <random>
 #include <string>
 #include <utility>
+
+#include "CardSet.h"
+
 using namespace std;
 
 
@@ -21,120 +24,6 @@ const string original_mountain = []() {  //建立原始牌山，將每種牌按�
     }
     return mountain;
 }();
-
-
-//建立一個CardSet類別，根據不同的選項可以添加、刪除或排序牌組。另外還有一些基本操作，如排序、輸出、清空、洗牌、添加和移除牌等功能。
-class CardSet {
-private:
-    //根據不同的選項修改牌組的函數，選項包括在牌組前面或後面添加牌、排序牌組、刪除指定數量的牌。
-    //option: 'b' = add to back, 'f' = add to front, 's' = sort, 'r' = remove
-    string mod_cards(string original_string, string target_string, char option, int count = 1) {
-        if (option == 'b') {
-            original_string += target_string;
-        }
-        else if (option == 'f') {
-            original_string = target_string + original_string;
-        }
-        else if (option == 's') {
-            original_string += target_string;
-            std::sort(original_string.begin(), original_string.end());
-        }
-        else if (option == 'r') {
-            string result = "";
-            bool remove_all = (count == -1);
-            for (char c : original_string) {
-                if (c == target_string[0] && (count > 0 || remove_all)) count--;
-                else result += c;
-            }
-            if (count > 0 && !remove_all) {
-                cout << "Warning: Not enough " << target_string[0] << " to remove." << endl;
-            }
-            original_string = result;
-        }
-        else {
-            cout << "Invalid option parameter for mod_cards." << endl;
-        }
-        return original_string;
-    }
-public:
-    int length;  //牌組的長度
-    string cards;  //牌組的內容
-
-    //建構函數，初始化牌組的長度和內容
-    CardSet(string initial_cards="") {
-        length = initial_cards.length();
-        cards = initial_cards;
-    }
-
-    //排序
-    void sort() {
-        std::sort(cards.begin(), cards.end());
-    }
-
-    //輸出到終端
-    void print() {
-        cout << cards << endl;
-    }
-
-    //清空
-    void clear() {
-        cards = "";
-        length = 0;
-    }
-
-    //設置牌組內容
-    void set(string new_cards) {
-        cards = new_cards;
-        length = cards.length();
-    }
-
-    //洗牌
-    void shuffle() {
-        static random_device random_device;
-        static mt19937 generator(random_device());
-        std::shuffle(cards.begin(), cards.end(), generator);
-    }
-
-    //添加指定字母牌至牌組，默認選項為'b'，表示將新的字母牌添加到牌組的末尾
-    void add(char card, char option = 'b') {
-        cards = mod_cards(cards, string(1, card), option);
-        length = cards.length();
-    }
-    void add(string new_cards, char option = 'b') {
-        cards = mod_cards(cards, new_cards, option);
-        length = cards.length();
-    }
-
-    //刪除牌組內容，默認刪除1張指定字母的牌
-    void remove(char card, int count = 1) {
-        cards = mod_cards(cards, string(1, card), 'r', count);
-        length = cards.length();
-    }
-
-    //將一段連續牌彈出成字串，並從原牌組移除，默認彈出1張牌，選項為'f'表示從牌組前面彈出，'b'表示從牌組後面彈出
-    string pop(int count = 1, char option = 'f') {
-        if (count > length) {
-            cout << "Warning: Not enough cards to pop." << endl;
-            count = length;
-        }
-        if (option == 'f') {
-            string popped_cards = cards.substr(0, count);
-            cards = cards.substr(count);
-            length = cards.length();
-            return popped_cards;
-        }
-        else if (option == 'b') {
-            string popped_cards = cards.substr(length - count, count);
-            cards = cards.substr(0, length - count);
-            length = cards.length();
-            return popped_cards;
-        }
-        else {
-            cout << "Invalid option parameter for pop." << endl;
-            return "";
-        }
-    }
-};
 
 
 //建立一個CardMountain類別，包含主要的牌山、寶牌指示牌、裡寶指示牌以及已開啟的寶牌指示牌數。該類別提供了重新生成牌山、獲取寶牌和裡寶指示牌以及開啟寶牌指示牌的功能。
